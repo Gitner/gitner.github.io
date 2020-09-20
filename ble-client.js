ae5529845fef1d89430bbc9b6a4b7ea9902b15f0
Gitner <spam@aruba.it>
2020-09-20 11:27:02 +0200

    Update ble-client.js

--- ble-client.js
+++ ble-client.js
@@ -27,10 +27,11 @@ class GateBLE {
     let options = {
       filters: [
         {
-          name: "RPiZeroW"
+          //name: "RPiZeroW"
+          services: 0xfff0
         }
       ],
-      optionalServices: [0xfff0]
+      //optionalServices: [0xfff0]
     };
     if (navigator.bluetooth == undefined) {
       alert("Sorry, Your device does not support Web BLE!");
