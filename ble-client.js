30d7106cd6d33cbe3b01e0d0b4f992c9d02e8f38
Gitner <spam@aruba.it>
2020-10-02 22:02:21 +0200

    Update ble-client.js

--- ble-client.js
+++ ble-client.js
@@ -48,7 +48,7 @@ class GateBLE {
 
   /* read UP state */
   async readUp() {
-    return await this.up.readValue()[0];
+    return await this.up.readValue();
   }
   
   /* write UP state */
@@ -58,7 +58,7 @@ class GateBLE {
   
   /* read DOWN state */
   async readDown() {
-    return await this.down.readValue()[0];
+    return await this.down.readValue();
   }
 
   /* write DOWN state */
